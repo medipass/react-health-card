35f4ef9a1d99db02b9e234cd410942ebf78fd679
jxom <jakemoxey@gmail.com>
2017-06-02 18:02:08 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# react-health-card
+
+> An awesome health card component.
+
+![](https://i.imgur.com/5SR05iw.gif)
+
+## Installation
+
+`yarn add react-health-card`
+
+or
+
+`npm install react-health-card --save`
+
+## Usage
+
+```javascript
+import HealthCard from 'react-health-card';
+
+<HealthCard
+  cardNumber="12345678"
+  name="Jake Moxey"
+  issueDate="12122020"
+  issueNumber="12"
+  rank="01"
+  memberNumber="87654321A"
+  memberNumberLength={9}
+  focused="memberNumber"
+  />
+```
+
+## Available Props
+
+| Property        | Type           | Default  | Description
+| ------------- |:-------------:| -----:|
+| bgColor | string | `#2053B1` | The background color of the card.
+| focused | string | `null` | The focused card attribute. Available: `null`, `rank`, `name`, `memberNumber`, `issueDate`, `issueNumber`, `cardNumber` 
+| isFlipped | boolean | `false` | Is the card flipped? 
+| type | string | `default` | Type of card.
+| logoUri | string | `null` | The logo of the card.
+| logoPosition | string | `front` | Position of the logo. Available: `front`, `back`
+| logoStyle | string | `right: 10%; top: 10%` | Style of the logo.
+| cardNumber | string | `null` | The card number.
+| cardNumberPosition | string | `front` | Position of the card number. Available: `front`, `back`
+| cardNumberTitle | string | `Card number` | Card number title
+| cardNumberLength | number | `8` | Card number length
+| cardNumberStyle | string | `left: 10%; top: 20%` | Style of the card number element (CSS)
+| issueDate | string | `null` | The card issue date.
+| issueDatePosition | string | `front` | Position of the issue date. Available: `front`, `back`
+| issueDateTitle | string | `Issue date` | The issue date title.
+| issueDateFormat | string | `DD/MM/YYYY` | The issue date format.
+| issueDateStyle | string | `bottom: 15%; left: 65%;` | Style of the issue date element (CSS)
+| issueNumber | string | `null` | The issue number.
+| issueNumberPosition | string | `back` | Position of the issue number. Available: `front`, `back`
+| issueNumberTitle | string | `Issue number` | Issue number title
+| issueNumberLength | number | `2` | Issue number length
+| issueNumberStyle | string | `left: 10%; bottom: 15%` | Style of the issue number element (CSS)
+| memberNumber | string | `null` | The member number.
+| memberNumberPosition | string | `front` | Position of the member number. Available: `front`, `back`
+| memberNumberTitle | string | `Member number` | Member number title
+| memberNumberLength | number | `8` | Member number length
+| memberNumberStyle | string | `left: 10%; bottom: 15%` | Style of the member number element (CSS)
+| rank | string | `null` | The card rank.
+| rankPosition | string | `front` | Position of the card rank. Available: `front`, `back`
+| rankLength | number | `2` | Card rank length
+| rankStyle | string | `left: 10%; bottom: 40%` | Style of the card rank element (CSS)
+| name | string | `null` | The card holder's name.
+| namePlaceholder | string | `FULL NAME` | Placeholder for card holder's name.
+| namePosition | string | `front` | Position of the card holder's name. Available: `front`, `back`
+| nameLength | number | `2` | Name length
+| nameStyle | string | `left: 20%; bottom: 40%` | Style of the card holder's name element (CSS)
+| showLogo | boolean | `true` | Shows the health card logo.
+| showCardNumber | boolean | `true` | Shows the card number.
+| showIssueDate | boolean | `true` | Shows the issue date.
+| showIssueNumber | boolean | `true` | Shows the issue number.
+| showMemberNumber | boolean | `true` | Shows the member number.
+| showRank | boolean | `true` | Shows the health card logo.
+| showName | boolean | `true` | Shows the health card logo.
+| showSwipeBar | boolean | `true` | Shows the swipe bar.
+
+## License
+
+MIT © [Jake Moxey](https://github.com/jxom)
---
+++ src/index.js
@@ -0,0 +1,354 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { lighten } from 'polished';
+import styled from 'styled-components';
+import _get from 'lodash/get';
+import _padEnd from 'lodash/padEnd';
+
+import images from './card-images';
+
+const Attribute = styled.div`${props => props.attributeStyle}`;
+const CardFront = styled.div`
+  ${props => props.bgColor ? `
+    background: linear-gradient(to right, ${lighten(0.2, props.bgColor)}, ${props.bgColor}) !important; 
+  ` : ''}
+`;
+const CardBack = CardFront;
+
+const _formatDate = (date, format) => {
+  const dateFormatArray = format.split('/');
+  let startIndex = 0;
+  let newDate = [];
+
+  dateFormatArray.forEach(element => {
+    const dateFormatElementLength = element.length;
+    const dateElement = date.slice(startIndex, startIndex + dateFormatElementLength);
+    startIndex += dateFormatElementLength;
+    newDate = [...newDate, _padEnd(dateElement || '', dateFormatElementLength, '•')];
+  });
+
+  return newDate.join('/');
+};
+
+export class Card extends Component {
+  getFocusedClass = attribute => {
+    const { focused } = this.props;
+    return (attribute === focused) ? 'react-health-card--focused' : '';
+  }
+
+  shouldCardBeFlipped = focused => {
+    const position = _get(this.props, `${focused}Position`);
+    return position === 'back';
+  }
+
+  handleToggleFlipCard = () => this.setState({ isFlipped: !this.state.isFlipped });
+
+  render = () => {
+    const {
+      bgColor,
+      logoUri,
+      focused,
+      isFlipped,
+      cardNumber,
+      issueDate,
+      issueNumber,
+      memberNumber,
+      name,
+      namePlaceholder,
+      rank,
+      type,
+      logoPosition,
+      logoStyle,
+      rankPosition,
+      rankStyle,
+      rankLength,
+      namePosition,
+      nameStyle,
+      memberNumberPosition,
+      memberNumberStyle,
+      memberNumberTitle,
+      memberNumberLength,
+      issueDateStyle,
+      issueDateTitle,
+      issueDatePosition,
+      issueDateFormat,
+      issueNumberPosition,
+      issueNumberStyle,
+      issueNumberTitle,
+      issueNumberLength,
+      cardNumberPosition,
+      cardNumberStyle,
+      cardNumberTitle,
+      cardNumberLength,
+      showLogo,
+      showCardNumber,
+      showIssueDate,
+      showIssueNumber,
+      showMemberNumber,
+      showRank,
+      showName,
+      showSwipeBar
+    } = this.props;
+    return (
+      <div className="react-health-card__container">
+        <div
+          className={
+            classNames(
+              'react-health-card',
+              type ? `react-health-card--${type}` : '',
+              (isFlipped || this.shouldCardBeFlipped(focused)) ? 'react-health-card--flipped' : ''
+            )
+          }
+          >
+          <CardFront className="react-health-card__front" bgColor={bgColor}>
+            {
+              logoPosition === 'front' && showLogo &&
+              <Attribute className="react-health-card__attribute" attributeStyle={logoStyle}>
+                <img
+                  src={_get(images, `${type}.logoUri`) || logoUri}
+                  className={classNames('react-health-card__logo', `react-health-card--${type}`)}
+                  />
+              </Attribute>
+            }
+            {
+              rankPosition === 'front' && showRank &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('rank'))}
+                attributeStyle={rankStyle}
+                >
+                {_padEnd(rank, rankLength, '•')}
+              </Attribute>
+            }
+            {
+              namePosition === 'front' && showName &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('name'))}
+                attributeStyle={nameStyle}
+                >
+                {name || namePlaceholder}
+              </Attribute>
+            }
+            {
+              memberNumberPosition === 'front' && showMemberNumber &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('memberNumber'))}
+                attributeStyle={memberNumberStyle}
+                >
+                <div className="react-health-card__with-content" data-before={memberNumberTitle}>{_padEnd(memberNumber, memberNumberLength, '•')}</div>
+              </Attribute>
+            }
+            {
+              issueDatePosition === 'front' && showIssueDate &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('issueDate'))}
+                attributeStyle={issueDateStyle}
+                >
+                <div className="react-health-card__with-content" data-before={issueDateTitle}>{_formatDate(issueDate, issueDateFormat)}</div>
+              </Attribute>
+            }
+            {
+              issueNumberPosition === 'front' && showIssueNumber &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('issueNumber'))}
+                attributeStyle={issueNumberStyle}
+                >
+                <div className="react-health-card__with-content" data-before={issueNumberTitle}>{_padEnd(issueNumber, issueNumberLength, '•')}</div>
+              </Attribute>
+            }
+            {
+              cardNumberPosition === 'front' && showCardNumber &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('cardNumber'))}
+                attributeStyle={cardNumberStyle}
+                >
+                <div className="react-health-card__with-content" data-before={cardNumberTitle}>{_padEnd(cardNumber, cardNumberLength, '•')}</div>
+              </Attribute>
+            }
+          </CardFront>
+          <CardBack className="react-health-card__back">
+            { showSwipeBar && <div className="react-health-card__bar"/> }
+            {
+              logoPosition === 'back' && showLogo &&
+              <Attribute className="react-health-card__attribute" attributeStyle={logoStyle}>
+                <img
+                  src={_get(images, `${type}.logoUri`) || logoUri}
+                  className={classNames('react-health-card__logo', `react-health-card--${type}`)}
+                  />
+              </Attribute>
+            }
+            {
+              rankPosition === 'back' && showRank &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('rank'))}
+                attributeStyle={rankStyle}
+                >
+                {_padEnd(rank, rankLength, '•')}
+              </Attribute>
+            }
+            {
+              namePosition === 'back' && showName &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('name'))}
+                attributeStyle={nameStyle}
+                >
+                {name || namePlaceholder}
+              </Attribute>
+            }
+            {
+              memberNumberPosition === 'back' && showMemberNumber &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('memberNumber'))}
+                attributeStyle={memberNumberStyle}
+                >
+                <div className="react-health-card__with-content" data-before={memberNumberTitle}>{_padEnd(memberNumber, memberNumberLength, '•')}</div>
+              </Attribute>
+            }
+            {
+              issueDatePosition === 'back' && showIssueDate &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('issueDate'))}
+                attributeStyle={issueDateStyle}
+                >
+                <div className="react-health-card__with-content" data-before={issueDateTitle}>{_formatDate(issueDate, issueDateFormat)}</div>
+              </Attribute>
+            }
+            {
+              issueNumberPosition === 'back' && showIssueNumber &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('issueNumber'))}
+                attributeStyle={issueNumberStyle}
+                >
+                <div className="react-health-card__with-content" data-before={issueNumberTitle}>{_padEnd(issueNumber, issueNumberLength, '•')}</div>
+              </Attribute>
+            }
+            {
+              cardNumberPosition === 'back' && showCardNumber &&
+              <Attribute
+                className={classNames('react-health-card__attribute react-health-card__editable', this.getFocusedClass('cardNumber'))}
+                attributeStyle={cardNumberStyle}
+                >
+                <div className="react-health-card__with-content" data-before={cardNumberTitle}>{_padEnd(cardNumber, cardNumberLength, '•')}</div>
+              </Attribute>
+            }
+          </CardBack>
+        </div>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  bgColor: PropTypes.string,
+  logoUri: PropTypes.string,
+  focused: PropTypes.string,
+  isFlipped: PropTypes.bool,
+  cardNumber: PropTypes.string,
+  issueDate: PropTypes.string,
+  issueNumber: PropTypes.string,
+  memberNumber: PropTypes.string,
+  namePlaceholder: PropTypes.string,
+  rank: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  logoPosition: PropTypes.string,
+  logoStyle: PropTypes.string,
+  rankPosition: PropTypes.string,
+  rankStyle: PropTypes.string,
+  rankLength: PropTypes.number,
+  namePosition: PropTypes.string,
+  nameStyle: PropTypes.string,
+  memberNumberPosition: PropTypes.string,
+  memberNumberStyle: PropTypes.string,
+  memberNumberTitle: PropTypes.string,
+  memberNumberLength: PropTypes.number,
+  issueDateStyle: PropTypes.string,
+  issueDateTitle: PropTypes.string,
+  issueDatePosition: PropTypes.string,
+  issueDateFormat: PropTypes.string,
+  issueNumberPosition: PropTypes.string,
+  issueNumberStyle: PropTypes.string,
+  issueNumberLength: PropTypes.number,
+  issueNumberTitle: PropTypes.string,
+  cardNumberPosition: PropTypes.string,
+  cardNumberStyle: PropTypes.string,
+  cardNumberTitle: PropTypes.string,
+  cardNumberLength: PropTypes.number,
+  showLogo: PropTypes.bool,
+  showCardNumber: PropTypes.bool,
+  showIssueDate: PropTypes.bool,
+  showIssueNumber: PropTypes.bool,
+  showMemberNumber: PropTypes.bool,
+  showRank: PropTypes.bool,
+  showName: PropTypes.bool,
+  showSwipeBar: PropTypes.bool
+};
+
+Card.defaultProps = {
+  bgColor: '#2053B1',
+  isFlipped: false,
+  focused: null,
+  logoUri: images.default.logoUri,
+  type: 'default',
+  rank: null,
+  name: null,
+  namePlaceholder: 'FULL NAME',
+  cardNumber: null,
+  memberNumber: null,
+  issueNumber: null,
+  issueDate: '',
+  issueDateFormat: 'DD/MM/YYYY',
+  logoStyle: `
+    right: 10%;
+    top: 10%;
+  `,
+  logoPosition: 'front',
+  issueDateStyle: `
+    bottom: 15%;
+    left: 65%;
+  `,
+  issueDatePosition: 'front',
+  issueDateTitle: 'Issue date',
+  issueNumberStyle: `
+    left: 10%;
+    bottom: 15%;
+  `,
+  issueNumberPosition: 'back',
+  issueNumberTitle: 'Issue number',
+  issueNumberLength: 2,
+  cardNumberStyle: `
+    left: 10%;
+    top: 20%;
+  `,
+  cardNumberPosition: 'front',
+  cardNumberTitle: 'Card number',
+  cardNumberLength: 8,
+  memberNumberStyle: `
+    bottom: 15%;
+    left: 10%;
+  `,
+  memberNumberPosition: 'front',
+  memberNumberTitle: 'Member number',
+  memberNumberLength: 8,
+  nameStyle: `
+    bottom: 40%;
+    left: 20%;
+  `,
+  namePosition: 'front',
+  rankStyle: `
+    bottom: 40%;
+    left: 10%;
+  `,
+  rankPosition: 'front',
+  rankLength: 2,
+  showLogo: true,
+  showCardNumber: true,
+  showIssueDate: true,
+  showIssueNumber: true,
+  showMemberNumber: true,
+  showRank: true,
+  showName: true,
+  showSwipeBar: true
+};
+
+export default Card;
